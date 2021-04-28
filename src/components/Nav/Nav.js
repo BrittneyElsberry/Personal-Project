2a3796946f80bf8ef6d00be23683dba7c0daa207
CoderBritt89 <72882552+CoderBritt89@users.noreply.github.com>
2021-04-28 14:28:15 -0600

    responsive design for navbar

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -4,6 +4,7 @@ import {logout} from '../../redux/authReducer'
 import {updateUser} from '../../redux/authReducer'
 import {Link, withRouter} from 'react-router-dom'
 import {GiHamburgerMenu} from 'react-icons/gi'
+import {TiDelete} from 'react-icons/ti'
 import './Nav.scss'
 
 
@@ -41,7 +42,14 @@ const Nav = (props)=> {
              >
             <GiHamburgerMenu color='white' size={28}/>  
             </button> 
+
+
               <div className={hambtn ? 'sliding-menu-container-active' : 'sliding-menu-container'}>
+
+              <button className={hambtn ? 'icon-btn-x' : 'icon-btn-none'}
+                            onClick={displaySlidingMenu}>
+                    <TiDelete size={28} margin={0} />            
+                    </button>
             <Link to="/managerview" ><a className='nav-item'>Manager View</a></Link> 
             <Link to="/deptdash"  ><a className='nav-item'>Department</a></Link>  
             <Link to='/companydash' ><a className='nav-item'>Company</a> </Link>
@@ -62,8 +70,15 @@ const Nav = (props)=> {
             >
             <GiHamburgerMenu color='white' size={28}/> 
             </button>
+
             
             <div className={hambtn ? 'sliding-menu-container-active' : 'sliding-menu-container'}>
+
+            <button className={hambtn ? 'icon-btn-x' : 'icon-btn-none'}
+                            onClick={displaySlidingMenu}>
+                    <TiDelete size={28} margin={0} />            
+            </button>
+
             <Link to="/deptdash"
                   onClick={displaySlidingMenu}
             ><a className='nav-item' alt='department dashboard'>Department</a></Link>  
